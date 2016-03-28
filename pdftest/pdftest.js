d860e3725c46cbb860275361060ffbf68cdcb5e7
Chad Watkins <cwatkins@doterra.com>
2016-03-28 13:55:43 -0600

    PDF test ajax2

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $( "#pdftest" ).click(function() {
     $.ajax({
          url: "http://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1&footer_text=%u",
-         type: GET,
+         type: "GET",
          beforeSend: function(xhr){xhr.setRequestHeader('Referer', 'http://http://chadwtkns.github.io/pdftest/pdftest.html');},
          success: function() { console.log('Success!'); }
       });
