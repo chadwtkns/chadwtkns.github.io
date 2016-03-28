70281f28bb3eb5a0065a827eb665e8a1811cdda2
Chad Watkins <cwatkins@doterra.com>
2016-03-28 15:13:33 -0600

    xhr expanded

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,47 +1,52 @@
 $(document).ready(function(){
+  // Create the XHR object.
   function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
     if ("withCredentials" in xhr) {
-
-      // Check if the XMLHttpRequest object has a "withCredentials" property.
-      // "withCredentials" only exists on XMLHTTPRequest2 objects.
+      // XHR for Chrome/Firefox/Opera/Safari.
       xhr.open(method, url, true);
-      xhr.onload = function() {
-       var responseText = xhr.responseText;
-       console.log(responseText);
-       // process the response.
-      };
-
-      xhr.onerror = function() {
-        console.log('There was an error!');
-      };
-
     } else if (typeof XDomainRequest != "undefined") {
-
-      // Otherwise, check if XDomainRequest.
-      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+      // XDomainRequest for IE.
       xhr = new XDomainRequest();
       xhr.open(method, url);
-
     } else {
-
-      // Otherwise, CORS is not supported by the browser.
+      // CORS not supported.
       xhr = null;
-      xhr.onload = function() {
-       var responseText = xhr.responseText;
-       console.log(responseText);
-       // process the response.
-      };
-
-      xhr.onerror = function() {
-        console.log('There was an error!');
-      };
     }
     return xhr;
   }
+
+  // Helper method to parse the title tag from the response.
+  function getTitle(text) {
+    return text.match('<title>(.*)?</title>')[1];
+  }
+
+  // Make the actual CORS request.
+  function makeCorsRequest() {
+    // All HTML5 Rocks properties support CORS.
+    var url = 'http://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1&footer_text=%u';
+
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+      alert('CORS not supported');
+      return;
+    }
+
+    // Response handlers.
+    xhr.onload = function() {
+      var text = xhr.responseText;
+      var title = getTitle(text);
+      alert('Response from CORS request to ' + url + ': ' + title);
+    };
+
+    xhr.onerror = function() {
+      alert('Woops, there was an error making the request.');
+    };
+
+    xhr.send();
+  }
   $( "#pdftest" ).click(function() {
-    createCORSRequest('GET','http://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1&footer_text=%u');
+    makeCorsRequest();
     // $.ajax({
     //      url: "http://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1&footer_text=%u",
     //      type: "GET",
