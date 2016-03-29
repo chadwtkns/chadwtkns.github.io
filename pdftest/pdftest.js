b14066de5d33a7d6e43bdef36fdf17ec6cd85283
Chad Watkins <cwatkins@doterra.com>
2016-03-29 14:35:42 -0600

    click event

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
   // Create the XHR object.
-
-  $( "#pdftest" ).click(function() {
-    // makeCorsRequest();
-    $.ajax({
-         url: "https://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1",
-         type: "GET",
-        //  complete: function(jqXhr,status) {
-        //    console.log(jqXhr);
-        //    console.log("Status " + status);
-        //  },
-         error: function(jqXhr, textStatus, error){
-           console.log(jqXhr);
-           console.log(textStatus);
-           console.log(error);
-         }
-      });
-  });
+$( "#pdftest" ).click();
+  // $( "#pdftest" ).click(function() {
+  //   // makeCorsRequest();
+  //   // $.ajax({
+  //   //      url: "https://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1",
+  //   //      type: "GET",
+  //   //     //  complete: function(jqXhr,status) {
+  //   //     //    console.log(jqXhr);
+  //   //     //    console.log("Status " + status);
+  //   //     //  },
+  //   //      error: function(jqXhr, textStatus, error){
+  //   //        console.log(jqXhr);
+  //   //        console.log(textStatus);
+  //   //        console.log(error);
+  //   //      }
+  //   //   });
+  // });
 });
