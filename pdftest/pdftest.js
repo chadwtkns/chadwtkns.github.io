23856efd5f4cab868842130f5a5849a259352223
Chad Watkins <cwatkins@doterra.com>
2016-03-29 12:10:09 -0600

    ajax call

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,57 +1,12 @@
 $(document).ready(function(){
   // Create the XHR object.
-  function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    if ("withCredentials" in xhr) {
-      // XHR for Chrome/Firefox/Opera/Safari.
-      xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-      // XDomainRequest for IE.
-      xhr = new XDomainRequest();
-      xhr.open(method, url);
-    } else {
-      // CORS not supported.
-      xhr = null;
-    }
-    return xhr;
-  }
 
-  // Helper method to parse the title tag from the response.
-  function getTitle(text) {
-    return text.match('<title>(.*)?</title>')[1];
-  }
-
-  // Make the actual CORS request.
-  function makeCorsRequest() {
-    // All HTML5 Rocks properties support CORS.
-    var url = 'http://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1&footer_text=%u';
-
-    var xhr = createCORSRequest('GET', url);
-    if (!xhr) {
-      alert('CORS not supported');
-      return;
-    }
-
-    // Response handlers.
-    xhr.onload = function() {
-      var text = xhr.responseText;
-      var title = getTitle(text);
-      alert('Response from CORS request to ' + url + ': ' + title);
-    };
-
-    xhr.onerror = function() {
-      alert('Woops, there was an error making the request.');
-    };
-
-    xhr.send();
-  }
   $( "#pdftest" ).click(function() {
-    makeCorsRequest();
-    // $.ajax({
-    //      url: "http://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1&footer_text=%u",
-    //      type: "GET",
-    //      success: function() { console.log('Success!'); }
-    //   });
+    // makeCorsRequest();
+    $.ajax({
+         url: "http://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1&footer_text=%u",
+         type: "GET",
+         success: function() { console.log('Success!'); }
+      });
   });
 });
