8cd00e86a2d04ccc6dbe76a04cb9ef60f874d51f
Chad Watkins <cwatkins@doterra.com>
2016-05-04 12:03:23 -0600

    jQuery Test fix

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,26 +1,3 @@
-// $(document).ready(function(){
-  // Create the XHR object.
-// $( "#pdftest" )[0].click(function () {
-//   console.log("something");
-// });
-  // $( "#pdftest" ).click(function() {
-  //   // makeCorsRequest();
-  //   // $.ajax({
-  //   //      url: "https://pdfcrowd.com/url_to_pdf/?height=11in&use_print_media=1",
-  //   //      type: "GET",
-  //   //     //  complete: function(jqXhr,status) {
-  //   //     //    console.log(jqXhr);
-  //   //     //    console.log("Status " + status);
-  //   //     //  },
-  //   //      error: function(jqXhr, textStatus, error){
-  //   //        console.log(jqXhr);
-  //   //        console.log(textStatus);
-  //   //        console.log(error);
-  //   //      }
-  //   //   });
-  // });
-});
-
 $( document ).ready(function() {
     $(".green").append("Some appended text.");
 });
