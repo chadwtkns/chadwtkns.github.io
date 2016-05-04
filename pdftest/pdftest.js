1cbd5d27caa2944a7b5cfc4195c34db5229b40eb
Chad Watkins <cwatkins@doterra.com>
2016-05-04 13:36:39 -0600

    onlyPrint css jquery option

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,9 +1,7 @@
 $( document ).ready(function() {
     $(".green").append("Some appended text.");
-    $("#pdftest").click(function () {
-      $.get("pdftest2.html", function (data) {
-        $(".green").append(data);
-      });
+    $.get("pdftest2.html", function (data) {
+      $(".onlyPrint").append(data);
     });
 
 });
