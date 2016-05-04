6c8627f74d25456a86a0cb2d4435a72aaea78f6b
Chad Watkins <cwatkins@doterra.com>
2016-05-04 12:23:38 -0600

    jQuery get test

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
     $(".green").append("Some appended text.");
-    $.get("http://www.google.com", function (data) {
+    $.get("pdftest2.html", function (data) {
       $(".green").html(data);
     });
 });
