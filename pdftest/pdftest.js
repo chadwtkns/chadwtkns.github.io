67b3e764d695c26736fc5d567c26081b35d8e181
Chad Watkins <cwatkins@doterra.com>
2016-05-05 09:48:36 -0600

    test links html

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   var href = [];
 
+  function getCallBack(data) {
+    $(".onlyPrint").append(data);
+  }
+
   $(".testLinks").each(function () {
     var something;
     something = $(this).attr("href");
     href.push(something);
   });
-  
+
   for (var i = 0; i < href.length; i++) {
-    $.get(href[i], function (data) {
-      $(".onlyPrint").append(data);
-    });
+    $.get(href[i], getCallBack);
   }
+  
 });
