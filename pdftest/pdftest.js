8cb82f40194bb076a878bbd1abdd653ab822a365
Chad Watkins <cwatkins@doterra.com>
2016-05-06 11:48:57 -0600

    click test

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,17 +1,34 @@
 $(document).ready(function() {
-  var href = [];
+  // var href = [];
+  //
+  // function getCallBack(data) {
+  //   $(".onlyPrint").append(data);
+  // }
+  //
+  // $(".testLinks").each(function () {
+  //   var something;
+  //   something = $(this).attr("href");
+  //   href.push(something);
+  // });
+  //
+  // for (var i = 0; i < href.length; i++) {
+  //   $.get(href[i], getCallBack);
+  // }
+  function BuildPDFCrowdLinks () {
+	var href = [];
 
-  function getCallBack(data) {
-    $(".onlyPrint").append(data);
+	$(".testLinks").each(function () {
+		href.push($(this).attr("href"));
+		var testIdea = $('<div class="visible-print-block"></div>');
+		$("#onlyPrint").append(testIdea);
+		for (var i = 0; i < href.length; i++) {
+			$(".visible-print-block").load(href[i] + " #qa-page");
+		}
+	});
+	CallPDFCrowd();
   }
-
-  $(".testLinks").each(function () {
-    var something;
-    something = $(this).attr("href");
-    href.push(something);
-  });
-
-  for (var i = 0; i < href.length; i++) {
-    $.get(href[i], getCallBack);
+  function CallPDFCrowd () {
+  	document.getElementById('listTopicPDFCrowd').click();
   }
+  $("#pdftest").off("click").on("click", ".dnldButtton", BuildPDFCrowdLinks);
 });
