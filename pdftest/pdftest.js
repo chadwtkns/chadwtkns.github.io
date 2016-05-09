b103085ab5159b68476adaf6b72f2883cdbd9936
Chad Watkins <cwatkins@doterra.com>
2016-05-09 11:08:11 -0600

    change link

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var href = [];
 
   function getCallBack(data) {
-    var thing = $(data).find('#printDiv')[0].innerHTML;
+    // var thing = $(data).find('#printDiv')[0].innerHTML;
     $(".onlyPrint").append(data);
   }
 
