bfe58ceee442118c80e77c5237c8eb3c562f56bb
Chad Watkins <cwatkins@doterra.com>
2016-05-09 10:58:44 -0600

    change link

--- pdftest/pdftest.js
+++ pdftest/pdftest.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
-  // var href = [];
-  //
-  // function getCallBack(data) {
-  //   $(".onlyPrint").append(data);
-  // }
-  //
-  // $(".testLinks").each(function () {
-  //   var something;
-  //   something = $(this).attr("href");
-  //   href.push(something);
-  // });
-  //
-  // for (var i = 0; i < href.length; i++) {
-  //   $.get(href[i], getCallBack);
-  // }
-  function BuildPDFCrowdLinks () {
-	var href = [];
+  var href = [];
 
-	$(".testLinks").each(function () {
-		href.push($(this).attr("href"));
-		var testIdea = $('<div class="visible-print-block"></div>');
-		$("#onlyPrint").append(testIdea);
-		for (var i = 0; i < href.length; i++) {
-			$(".visible-print-block").load(href[i] + " #printDiv");
-		}
-	});
-	CallPDFCrowd();
+  function getCallBack(data) {
+    $(".onlyPrint").append(data);
   }
-  function CallPDFCrowd () {
-  	document.getElementById('listTopicPDFCrowd').click();
+
+  $(".testLinks").each(function () {
+    var something;
+    something = $(this).attr("href");
+    href.push(something);
+  });
+
+  for (var i = 0; i < href.length; i++) {
+    $.get(href[i], getCallBack);
   }
-  $("#pdftest").off("click").on("click", ".dnldButtton", BuildPDFCrowdLinks);
+  // function BuildPDFCrowdLinks () {
+	// var href = [];
+  //
+	// $(".testLinks").each(function () {
+	// 	href.push($(this).attr("href"));
+	// 	var testIdea = $('<div class="visible-print-block"></div>');
+	// 	$("#onlyPrint").append(testIdea);
+	// 	for (var i = 0; i < href.length; i++) {
+	// 		$(".visible-print-block").load(href[i] + " #printDiv");
+	// 	}
+	// });
+	// CallPDFCrowd();
+  // }
+  // function CallPDFCrowd () {
+  // 	document.getElementById('listTopicPDFCrowd').click();
+  // }
+  // $("#pdftest").off("click").on("click", ".dnldButtton", BuildPDFCrowdLinks);
 });
